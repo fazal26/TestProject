376dcc3d33db9012171c0b8d65ac0f88fee0b124
Fazal Karim <meetfazalkarim@gmail.com>
2019-01-29 11:40:43 +0500

    admin seeded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
     include Pundit
 
+    # def after_sign_in_path_for(resource)
+    #  puts "(((((((((((((((((("*100
+    #  resource.next_step
+    #  puts "(((((((((((((((((("*100
+    # # new_case_path and return
+    # end
     def after_sign_in_path_for(resource)
-     puts "(((((((((((((((((("*100
-     puts resource.inspect
-     puts "(((((((((((((((((("*100
-    # new_case_path and return
-    end
+        if current_user.has_role?(:admin)
+          puts "(((((((((((((((((("*100
+         new_case_path
+        elsif current_user.has_role?(:user)
+         root_path
+        else
+         root_path
+        end
+       end
 end
