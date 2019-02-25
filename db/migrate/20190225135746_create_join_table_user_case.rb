3feb4c2938e735d44304022b92b40a8456c1fc47
Fazal Karim <meetfazalkarim@gmail.com>
2019-02-25 20:03:15 +0500

    no join table migration

--- db/migrate/20190225135746_create_join_table_user_case.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUserCase < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :users, :cases do |t|
-      # t.index [:user_id, :case_id]
-      # t.index [:case_id, :user_id]
-    end
-  end
-end
