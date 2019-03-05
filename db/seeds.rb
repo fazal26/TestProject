dff6ac4b5e233a79cde0bfc996bf38163d6d5995
Fazal Karim <meetfazalkarim@gmail.com>
2019-03-05 17:43:18 +0500

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ Category.create!([
 
 user = User.new({
     email: "admin@admin.com",
-    username: "Super Cool Admin"
+    username: "Super Cool Admin",
     password: "111111",
-    password_confirmation: "111111",
+    password_confirmation: "111111"
 })
 
 user.add_role(:super)
